285cb5c9f51e571d848835447d45ffcd20b082f7
Awetb93 <58653271+Awetb93@users.noreply.github.com>
2021-07-19 11:39:19 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import express,{Request,Response,Application} from "express"
+const app= express()
+const port = process.env.PORT || 5000
+const sum=(a:number,b:number)=>a+b
+app.get("/",(req:Request,res:Response)=>res.send("welcome"))
+app.listen(port,()=>console.log("welcome",sum(4,5)))
